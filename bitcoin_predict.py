77d0483395768687299757f3c31a251b25b0ea21
Damon <damon@example.com>
2018-04-24 22:36:41 +0800

    成功模拟出正相关关系

--- bitcoin_predict.py
+++ bitcoin_predict.py
@@ -14,13 +14,15 @@ header_row = next(reader)
 cap = []
 date = []
 for row in reader:
-    cap1 = float(row[1]) / 1000000000
+    cap1 = int(float(row[1]) / 1000000000)
     _date = datetime.strptime(row[0], "%Y-%m-%d")
     cap.append(cap1)
     date.append(_date)
 
 cap.reverse()
+# print(cap)
 date.reverse()
+# print(date)
 
 f1 = open(filename2)
 reader1 = csv.reader(f1)
@@ -28,19 +30,20 @@ header_row1 = next(reader1)
 # print(header_row)
 
 address = []
-date1 = []
+# date1 = []
 for row1 in reader1:
-    _address = math.pow(float(row1[1]), 2 ** 0.5)
-    _date1 = datetime.strptime(row1[0], "%Y/%m/%d")
+    _address = 88 * (math.pow(float(row1[1])/10000, 2))
+    # _date1 = datetime.strptime(row1[0], "%Y/%m/%d")
     address.append(_address)
-    date1.append(_date1)
+    # date1.append(_date1)
 
 address.reverse()
-date1.reverse()
+# date1.reverse()
+print(address)
 
 fig = plt.figure(dpi=128, figsize=(5, 3))
-plt.plot(date, cap, c='red')
-plt.plot(date, address, c='blue')
+plt.plot(date, cap, c='blue')
+plt.plot(date, address, c='black')
 plt.title("Bitcoin Cap", fontsize=10)
 plt.xlabel('', fontsize=8)
 fig.autofmt_xdate()
